2cf25c515a00a290cacd50be28752fa2860ce886
jirgens <jessa.irg@gmail.com>
2014-09-24 11:33:15 -0700

    seeded

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+User.create(:id => 1, :email => 'orlene@laughingwithflowers.com', :password => 'winnielou')
+
+User.create(:id => 2, :email => 'jessa.irg@gmail.com', :password => 'brooklyn')
+
+welcome = Post.create(:id => 1, :title => 'Welcome to Laughing With Flowers!', :body => 'This is my fist post on laughingwithflowers.com. Please check back often to see what I am up to!')
+welcome.created_at =  (rand*10).days.ago
+welcome.save
+
+second = Post.create(:id => 2, :title => 'Just testing to see if Post works!', :body => 'Edit me!. Please check back often to see what I am up to!')
+second.created_at =  (rand*2).days.ago
+second.save
